cfb9fb621b6894ae9ad19ff53a400d5381043f22
Jonathan James Smithers <jsmithe2@gmu.edu>
2014-08-10 18:08:38 -0400

    Make new view available
    Namely, "add_contact", for adding someone to your keyring.

--- popup.js
+++ popup.js
@@ -1,27 +1,46 @@
-// alert('hi')
-// alert($('#popup').selected())
-// $('.btn').on('click', function({this.addClass('active')}));
 $(function() {
-	$("body").on("click", ".btn", function() { //https://github.com/twbs/bootstrap/issues/2380
+	// VARIABLES
+	KEYRING_ADD = $("#keys_add");
+	KEYRING_MANAGE = $("#keys_manage");
+	USEWITH_FACEBOOK = $("#facebook");
+
+	// GROUPED-BUTTON CLICKS
+	// Here's why I detect clicks on the body: https://github.com/twbs/bootstrap/issues/2380
+	$("body").on("click", ".btn", function() { 
 		el = $(this).children();
 		group = el.attr('name');
 		if (group == "display") {
-			console.log('display')
 			method = el.attr('id').slice(2)
 			chrome.storage.local.set({"displayMethod":method})
 		} else if (group == "edit") {
-			console.log('edit')
 			method = el.attr('id').slice(2)
 			chrome.storage.local.set({"editMethod":method})
 		} 
 	})
 
-	$('#facebook').on('click', function() {
+	KEYRING_ADD.on('click', function() {
+		//open view as popup
+		var w = 405
+		var h = 555 //655
+		var left = Math.round((window.screen.width - w) / 2);
+		var top = Math.round((window.screen.height - h) / 2);
+		var windowUrl = chrome.extension.getURL('views/add_contact/add_contact.html')
+		var windowOptions = {
+			url: windowUrl, 
+			width: w, 
+			height: h, 
+			left: left, 
+			top: top, 
+			focused:true, 
+			type:"popup"
+		};
+		chrome.windows.create(windowOptions);
+	})
+	USEWITH_FACEBOOK.on('click', function() {
 		ischecked = $(this).is(':checked')
 		chrome.storage.local.set({"facebook": ischecked})
 	})
 
-	console.log("here")
 	chrome.storage.local.get(["displayMethod", "editMethod", "facebook"], function(settings) {
 		display = settings.displayMethod
 		edit = settings.editMethod
---
+++ views/add_contact/add_contact.js
@@ -0,0 +1,8 @@
+		// var w = 405 //291;
+		// var h = 250 //180;
+		// var left = (window.screen.width)-((w)+10);
+		// var top = 25+10; //(window.screen.height/2)-(h/2);
+		// var url = chrome.extension.getURL('safe_view/view.html')
+		// var options = {url: url, width: w, height: h, left: left, top: top, focused:true, type:"popup"};
+		// console.log(options);
+		// chrome.windows.create(options);
