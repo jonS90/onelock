e73b35c2bd50c62cd567be7b813bd5d14b685402
Jonathan James Smithers <jsmithe2@gmu.edu>
2014-08-23 20:45:23 -0400

    Use placeholder in place of javascript
    Let's put HTML5 to use, please. No need for all that convoluted javascript.

--- views/add_contact/add_contact.js
+++ views/add_contact/add_contact.js
@@ -5,44 +5,12 @@ $(function() {
 	SUBMIT_BUTTON = $(".btn");
 	DEFAULT_NAME_VALUE = CONTACT_NAME.val();
 	DEFAULT_KEY_VALUE = CONTACT_KEY.val(); 
-	DEFAULT_COLOR = CONTACT_NAME.css('color');
-	GRAYED_COLOR = "gray";
 	STATUS = $("#status");
 
-	CONTACT_NAME.css('color', GRAYED_COLOR);
-	CONTACT_KEY.css('color', GRAYED_COLOR);
 	SUBMIT_BUTTON.prop('disabled', true);
 
 	//for testing:
 	CONTACT_KEY.val("-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzoWIXqCGKrIeFTHH2p5k\nzK6x3eOgFcaQLu7BVmwugZbvTseQoTrQx/wOXLh0G3fGb+jQTIcsCl57mxh2lHNX\nfkCJK5u4G+0WkwTS1AcdmqvQuhyyPg0+HbZahk4HwbbXFZWdHVHvelKcR1Zn3mKZ\nZmDWSLqHHbQpN1tu/u2IcbmG6uMPoK8aiVF2L0SHVtw/QRnXP5PKDz7pqmc1KieT\nC0MRRKV4nJ4L04rQind5UiiQFpF/LOBYNUFs3df9Pr2Yf9Xz3sdY87hP9h/HDvxU\nv9s8sFirV1npomHGCIG+MZVBU1ehzPbELpyo3i13oEi2SrVduLw1lJkO4NjwO4c1\noQIDAQAB\n-----END PUBLIC KEY-----");
-
-
-	CONTACT_NAME.on('focus', function() {
-		if (CONTACT_NAME.val() == DEFAULT_NAME_VALUE) {
-			CONTACT_NAME.val("");
-			CONTACT_NAME.css('color', DEFAULT_COLOR)
-		}
-	})
-	CONTACT_NAME.on('blur', function() {
-		if (CONTACT_NAME.val() == "") {
-			CONTACT_NAME.val(DEFAULT_NAME_VALUE);
-			CONTACT_NAME.css('color', GRAYED_COLOR);
-		}
-	})
-
-	CONTACT_KEY.on('focus', function() {
-		if (CONTACT_KEY.val() == DEFAULT_KEY_VALUE) {
-			CONTACT_KEY.val("");
-			CONTACT_KEY.css('color', DEFAULT_COLOR);
-		}
-	})
-	CONTACT_KEY.on('blur', function() {
-		if (CONTACT_KEY.val() == "") {
-			CONTACT_KEY.val(DEFAULT_KEY_VALUE);
-			CONTACT_KEY.css('color', GRAYED_COLOR);
-		}
-	})
-
 
 	//form validation
 	CONTACT_KEY.on('change', onchange);
